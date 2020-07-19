482721b1476cb4ac5e4caf9e006fdcbde71c816e
phamvanhieu3012 <phamvanhieu74123@gmail.com>
2020-07-19 11:02:11 +0700

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import TodoList from './components/TotoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love Easy Frontend! 😍 ' },
+    { id: 2, title: 'We love Easy Frontend! 🥰 ' },
+    { id: 3, title: 'They love Easy Frontend! 🚀 ' },
+  ]);
+
   return (
     <div className="app">
-      <h1>Welcome to react hooks</h1>
+      <h1>React-hooks TodoList</h1>
 
-      <ColorBox />
+      <TodoList todos={todoList} />
     </div>
   );
 }
